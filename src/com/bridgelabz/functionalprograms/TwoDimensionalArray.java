d32e52a1417048a6b818baa35ae9f7ab37328722
Prashanth N <87635664+prash49@users.noreply.github.com>
2021-08-29 13:01:17 +0530

    Modified: updated the 2D array
    Updated the 2d array program using method

--- src/com/bridgelabz/functionalprograms/TwoDimensionalArray.java
+++ src/com/bridgelabz/functionalprograms/TwoDimensionalArray.java
@@ -3,67 +3,78 @@ package com.bridgelabz.functionalprograms;
 import java.util.Scanner;
 
 public class TwoDimensionalArray {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static int[][] arrayInt(int size1, int size2) {
+        int[][] intArray = new int[size1][size2];
+        System.out.println("enter array elements");
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                intArray[i][j] = scanner.nextInt();
+            }
+        }
+        return intArray;
+    }
+
+    public static double[][] arrayDouble(int size1, int size2) {
+        double[][] doubleArray = new double[size1][size2];
+        System.out.println("enter array elements");
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                doubleArray[i][j] = scanner.nextDouble();
+            }
+        }
+        return doubleArray;
+    }
+
+    public static boolean[][] arrayBoolean(int size1, int size2) {
+        boolean[][] booleanArray = new boolean[size1][size2];
+
+        System.out.println("enter array elements");
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                booleanArray[i][j] = scanner.nextBoolean();
+            }
+        }
+        return booleanArray;
+    }
 
     public static void main(String[] args) {
         System.out.println("Enter the data type of array");
         Scanner scanner = new Scanner(System.in);
         String type = scanner.nextLine();
+        System.out.println(" enter the size  2D array");
+        int size1 = scanner.nextInt();
+        int size2 = scanner.nextInt();
         switch (type) {
             case "int" -> {
-                System.out.println(" enter the size  2D array");
-                int size1 = scanner.nextInt();
-                int size2 = scanner.nextInt();
-                int[][] intArray = new int[size1][size2];
-                System.out.println("enter array elements");
-                for (int i = 0; i < size1; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        intArray[i][j] = scanner.nextInt();
-                    }
-                }
+                int array1[][] = arrayInt(size1, size2);
                 System.out.println("Array Elements are");
                 for (int i = 0; i < size1; i++) {
                     for (int j = 0; j < size2; j++) {
-                        System.out.print(intArray[i][j]);
+                        System.out.print(array1[i][j]);
                         System.out.print("  ");
                     }
                     System.out.println();
                 }
             }
             case "double" -> {
-                System.out.println(" enter the size  2D array");
-                int size3 = scanner.nextInt();
-                int size4 = scanner.nextInt();
-                double[][] doubleArray = new double[size3][size4];
-                System.out.println("enter array elements");
-                for (int i = 0; i < size3; i++) {
-                    for (int j = 0; j < size4; j++) {
-                        doubleArray[i][j] = scanner.nextDouble();
-                    }
-                }
+                double[][] array2 = arrayDouble(size1, size2);
                 System.out.println("Array Elements are");
-                for (int i = 0; i < size3; i++) {
-                    for (int j = 0; j < size4; j++) {
-                        System.out.print(doubleArray[i][j]);
+                for (int i = 0; i < size1; i++) {
+                    for (int j = 0; j < size2; j++) {
+                        System.out.print(array2[i][j]);
                         System.out.print("  ");
                     }
                     System.out.println();
                 }
             }
             case "boolean" -> {
-                System.out.println(" enter the size  2D array");
-                int size5 = scanner.nextInt();
-                int size6 = scanner.nextInt();
-                boolean[][] booleanArray = new boolean[size5][size6];
-                System.out.println("enter array elements");
-                for (int i = 0; i < size5; i++) {
-                    for (int j = 0; j < size6; j++) {
-                        booleanArray[i][j] = scanner.nextBoolean();
-                    }
-                }
+                boolean[][] array3 = arrayBoolean(size1, size2);
                 System.out.println("Array Elements are");
-                for (int i = 0; i < size5; i++) {
-                    for (int j = 0; j < size6; j++) {
-                        System.out.print(booleanArray[i][j]);
+                for (int i = 0; i < size1; i++) {
+                    for (int j = 0; j < size2; j++) {
+                        System.out.print(array3[i][j]);
                         System.out.print("  ");
                     }
                     System.out.println();
